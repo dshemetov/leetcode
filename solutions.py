40a35479e7e8bae1a5c5cffff11c16853df349f2
Dmitry Shemetov <dshemetov@ucdavis.edu>
2023-04-30 00:51:52 -0700

    feat: solve problem 587

--- solutions.py
+++ solutions.py
@@ -1398,69 +1398,134 @@ def minMutation(startGene: str, endGene: str, bank: list[str]) -> int:
     return -1
 
 
-# %% 587. https://leetcode.com/problems/erect-the-fence/
+# %% 587. Erect the Fence https://leetcode.com/problems/erect-the-fence/
 # Lessons learned:
-# - The atan2 function was invented back in the Fortran days and makes for a stable polar angle definition
-# - Convex hull: https://en.wikipedia.org/wiki/Convex_hull_algorithms#Algorithms
-# - Graham scan: https://en.wikipedia.org/wiki/Graham_scan
+# - A broad class of computational geometry algorithms solve this: https://en.wikipedia.org/wiki/Convex_hull_algorithms#Algorithms
+# - The Graham scan is easy to understand and decently fast: https://en.wikipedia.org/wiki/Graham_scan
 # - Tip from a graphics guy: avoid representing angles with degrees/radians, stay in fractions
-def crossproduct_vec(v1: tuple[int, int], v2: tuple[int, int]) -> int:
-    return v1[0] * v2[1] - v1[1] * v2[0]
+# - The atan2 function was invented back in the Fortran days and makes for a stable polar angle definition
+# - The edge-cases of the Graham scan are tricky, especially all the cases with colinear points
+def ccw(p1: tuple[int, int], p2: tuple[int, int], p3: tuple[int, int]) -> float:
+    """
+    Examples:
+    >>> ccw((0, 0), (1, 0), (0, 1))
+    1.0
+    >>> ccw((0, 0), (1, 0), (1, 1))
+    1.0
+    >>> ccw((0, 0), (1, 0), (1, 0))
+    0.0
+    >>> ccw((0, 0), (1, 0), (0, -1))
+    -1.0
+    """
+    v1 = (p2[0] - p1[0], p2[1] - p1[1])
+    v2 = (p3[0] - p2[0], p3[1] - p2[1])
+    return float(v1[0] * v2[1] - v1[1] * v2[0])
 
-def crossproduct_pt(pt1: tuple[int, int], pt2: tuple[int, int], pt3: tuple[int, int]) -> float:
-    v1 = (pt2[0] - pt1[0], pt2[1] - pt1[1])
-    v2 = (pt3[0] - pt2[0], pt3[1] - pt2[1])
-    return crossproduct_vec(v1, v2)
 
-def polar_angle(pt1: tuple[int, int], pt2: tuple[int, int]) -> float:
-    v1 = (pt2[0] - pt1[0], pt2[1] - pt1[1])
+def polar_angle(p1: tuple[int, int], p2: tuple[int, int]) -> float:
+    """Get the polar angle of the vector from p1 to p2."""
+    v1 = (p2[0] - p1[0], p2[1] - p1[1])
     return np.arctan2(v1[1], v1[0])
+
+
+def point_sorter(p1: tuple[int, int], p2: tuple[int, int]) -> float:
+    """Sort by polar angle and break ties by distance."""
+    return (polar_angle(p1, p2), -np.linalg.norm((p2[0] - p1[0], p2[1] - p1[1])))
+
+
+def atan2notan(y: int, x: int) -> Fraction:
+    """A polar angle substitute without trigonometry or floating points.
+
+    Imagine tracing out a circle counterclockwise and measuring the angle to the tracing vector
+    from the positive x axis. This is the sorted order we wish to achieve. This function will give
+    a lexically smaller tuple for smaller angles.
+    """
+    if x == 0 and y == 0:
+        return (0, 0)
+    elif x > 0 and y >= 0:
+        return (0, Fraction(y, x))
+    elif x == 0 and y > 0:
+        return (1, 0)
+    elif x < 0:
+        return (2, Fraction(y, x))
+    elif x == 0 and y < 0:
+        return (3, 0)
+    elif x > 0 and y < 0:
+        return (4, Fraction(y, x))
+    else:
+        raise ValueError("How did you even get here?")
+
+
+def partition_by(l: list, f: Callable) -> dict:
+    """Partition a list into lists based on a predicate."""
+    d = defaultdict(list)
+    for item in l:
+        d[f(item)].append(item)
+    return d
+
+
+def plot_points(points: list[tuple[int, int]], hull: list[tuple[int, int]]):
+    import matplotlib.pyplot as plt
+
+    x, y = zip(*points)
+    plt.scatter(x, y)
+    x, y = zip(*hull)
+    plt.plot(x, y, color="green")
+    plt.show()
+
 
 def outerTrees(trees: list[list[int]]) -> list[list[int]]:
     """
+    We are going to use a Graham scan to find the convex hull of the points.
+
     Examples:
     >>> outerTrees([[1,1],[2,2],[2,0],[2,4],[3,3],[4,2]])
-    [[1, 1], [2, 0], [3, 3], [2, 4], [4, 2]]
+    [[2, 0], [4, 2], [3, 3], [2, 4], [1, 1]]
     >>> outerTrees([[1,2],[2,2],[4,2]])
     [[1, 2], [2, 2], [4, 2]]
     >>> outerTrees([[1,1],[2,2],[3,3],[2,1],[4,1],[2,3],[1,3]])
-    [[1, 1], [2, 1], [4, 1], [3, 3], [1, 3]]
+    [[1, 1], [2, 1], [4, 1], [3, 3], [2, 3], [1, 3]]
+    >>> outerTrees([[3,0],[4,0],[5,0],[6,1],[7,2],[7,3],[7,4],[6,5],[5,5],[4,5],[3,5],[2,5],[1,4],[1,3],[1,2],[2,1],[4,2],[0,3]])
+    [[3, 0], [4, 0], [5, 0], [6, 1], [7, 2], [7, 3], [7, 4], [6, 5], [5, 5], [4, 5], [3, 5], [2, 5], [1, 4], [0, 3], [1, 2], [2, 1]]
+    >>> outerTrees([[0,2],[0,1],[0,0],[1,0],[2,0],[1,1]])
+    [[0, 0], [1, 0], [2, 0], [1, 1], [0, 2], [0, 1]]
+    >>> outerTrees([[0,2],[0,4],[0,5],[0,9],[2,1],[2,2],[2,3],[2,5],[3,1],[3,2],[3,6],[3,9],[4,2],[4,5],[5,8],[5,9],[6,3],[7,9],[8,1],[8,2],[8,5],[8,7],[9,0],[9,1],[9,6]])
+    [[9, 0], [9, 1], [9, 6], [7, 9], [5, 9], [3, 9], [0, 9], [0, 5], [0, 4], [0, 2], [2, 1]]
+    >>> outerTrees([[0,0],[0,1],[0,2],[1,2],[2,2],[3,2],[3,1],[3,0],[2,0],[1,0],[1,1],[3,3]])
+    [[0, 0], [1, 0], [2, 0], [3, 0], [3, 1], [3, 2], [3, 3], [0, 2], [0, 1]]
     """
     lowest_left_point = (math.inf, math.inf)
-    for x, y in pts:
+    for x, y in trees:
         if y < lowest_left_point[1] or (y == lowest_left_point[1] and x < lowest_left_point[0]):
             lowest_left_point = (x, y)
 
-    # Sort by polar angle.
-    trees = sorted(trees, key=lambda x: polar_angle(lowest_left_point, x))
+    trees_by_slope = partition_by(trees, lambda p: atan2notan(p[1] - lowest_left_point[1], p[0] - lowest_left_point[0]))
+    slopes = sorted(trees_by_slope.keys())
 
+    # Handles many colinear cases; order doesn't matter for leetcode
+    if len(slopes) == 1:
+        return trees
 
+    def distance(p1, p2):
+        return np.linalg.norm((p1[1] - p2[1], p1[0] - p2[0]))
 
-    # stack = []
-    # new_pt = trees[1]
-    # while new_pt != start:
+    # The right-most line should sort by increasing distance from lowest left point
+    trees_by_slope[slopes[0]] = sorted(trees_by_slope[slopes[0]], key=lambda p: distance(p, lowest_left_point))
+    # The left-most line should sort by decreasing distance from lowest left point
+    trees_by_slope[slopes[-1]] = sorted(trees_by_slope[slopes[-1]], key=lambda p: -distance(p, lowest_left_point))
+    # The rest should sort by increasing distance from lowest left point
+    for slope in slopes[1:-1]:
+        trees_by_slope[slope] = sorted(trees_by_slope[slope], key=lambda p: distance(p, lowest_left_point))
 
-    # def find_minimal_slope(pt: tuple[int, int], slope: float) -> tuple[int, int]:
-    #     min_slope = slope
-    #     for tree in trees:
-    #         if pt[0] == tree[0]:
+    stack = []
+    for slope in slopes:
+        for tree in trees_by_slope[slope]:
+            while len(stack) >= 2 and ccw(stack[-2], stack[-1], tree) < 0:
+                stack.pop()
+            stack.append(tree)
 
-    # between_trees = []
+    return stack
 
-pts = [(math.cos(t), math.sin(t)) for t in np.linspace(0, 2 * math.pi, 20)]
-lowest_left_point = (math.inf, math.inf)
-for x, y in pts:
-    if y < lowest_left_point[1] or (y == lowest_left_point[1] and x < lowest_left_point[0]):
-        lowest_left_point = (x, y)
-
-sorted_pts = sorted(pts, key=lambda x: polar_angle(lowest_left_point, x))
-sorted_pts
-
-# %%
-lowest_left_point
-
-# %%
-crossproduct_pt(lowest_left_point, )
 
 # %% 622. Design Circular Queue https://leetcode.com/problems/design-circular-queue/
 _empty = object()
