72bd79a938b14a27444b6c5a89aaa084bed734df
Dmitry Shemetov <dshemetov@ucdavis.edu>
2023-04-30 00:58:23 -0700

    feat: solve problem 1046

--- solutions.py
+++ solutions.py
@@ -1913,7 +1913,25 @@ def equationsPossible(equations: list[str]) -> bool:
     return True
 
 
-# %% 1047. https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
+# %% 1046. Last Stone Weight https://leetcode.com/problems/last-stone-weight/
+def lastStoneWeight(stones: list[int]) -> int:
+    """
+    Examples:
+    >>> lastStoneWeight([2,7,4,1,8,1])
+    1
+    >>> lastStoneWeight([1,3])
+    2
+    >>> lastStoneWeight([1])
+    1
+    """
+    sorted_stones = sorted(stones)
+    while len(sorted_stones) > 1:
+        a, b = sorted_stones.pop(), sorted_stones.pop()
+        if a != b:
+            insort(sorted_stones, a - b)
+    return sorted_stones[0] if sorted_stones else 0
+
+
 def remove_duplicates(s: str) -> str:
     """
     Examples:
