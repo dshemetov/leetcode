28ed3220d840b05101aeda1961d83c9d0a1f2194
Dmitry Shemetov <dshemetov@ucdavis.edu>
2023-04-30 00:44:27 -0700

    feat: problem 5

--- solutions.py
+++ solutions.py
@@ -238,6 +238,68 @@ def get_median_sorted(nums: list[int]) -> float:
         return nums[mid]
 
 
+# %% 5. Longest Palindromic Substring https://leetcode.com/problems/longest-palindromic-substring/
+# Lessons learned:
+# - In longestPalindrome, I tried an approach with three pointers and expanding outwards if the characters
+#   matched. The edge case I couldn't figure out is long runs of the same character. The issue there is that
+#   you need to keep changing the palindrome center. I gave up on that approach and looked at the solution.
+# - The expand_center solution is straightforward and clear and I probably would have thought of it, if I 
+#   didn't get stuck trying to fix the three pointer approach.
+def longestPalindrome(s: str) -> str:
+    """
+    Examples:
+    >>> longestPalindrome("babad")
+    'bab'
+    >>> longestPalindrome("cbbd")
+    'bb'
+    >>> longestPalindrome("ac")
+    'a'
+    >>> longestPalindrome("abcde")
+    'a'
+    >>> longestPalindrome("abcdeedcba")
+    'abcdeedcba'
+    >>> longestPalindrome("abcdeeffdcba")
+    'ee'
+    >>> longestPalindrome("abaaba")
+    'abaaba'
+    >>> longestPalindrome("abaabac")
+    'abaaba'
+    >>> longestPalindrome("aaaaa")
+    'aaaaa'
+    >>> longestPalindrome("aaaa")
+    'aaaa'
+    """
+    if len(s) == 1:
+        return s
+
+    lo, hi = 0, 1
+    max_length = 1
+    max_length_location = None
+
+    def expand_center(lo, hi):
+        while lo >= 0 and hi < len(s) and s[lo] == s[hi]:
+            lo -= 1
+            hi += 1
+        return lo + 1, hi - 1
+
+    for i in range(1, len(s)):
+        lo, hi = expand_center(i - 1, i + 1)
+        if hi - lo + 1 > max_length:
+            max_length = hi - lo + 1
+            max_length_location = lo, hi
+
+        lo, hi = expand_center(i - 1, i)
+        if hi - lo + 1 > max_length:
+            max_length = hi - lo + 1
+            max_length_location = lo, hi
+
+    if max_length == 1:
+        return s[0]
+    else:
+        lo, hi = max_length_location
+        return s[lo:hi+1]
+
+
 # %% 8. String to Integer (atoi) https://leetcode.com/problems/string-to-integer-atoi/
 def my_atoi(s: str) -> int:
     """8. String to Integer (atoi) https://leetcode.com/problems/string-to-integer-atoi/
