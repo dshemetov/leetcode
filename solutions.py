826d0a0117fc9d17a02815c2c2de6b7f2d6968cb
Dmitry Shemetov <dshemetov@ucdavis.edu>
2023-05-13 23:14:27 -0700

    feat: solve problem 495

--- solutions.py
+++ solutions.py
@@ -1519,6 +1519,22 @@ def minMutation(startGene: str, endGene: str, bank: list[str]) -> int:
     return -1
 
 
+# %% 495. Teemo Attacking https://leetcode.com/problems/teemo-attacking
+def findPoisonedDuration(timeSeries: list[int], duration: int) -> int:
+    """
+    Examples:
+    >>> findPoisonedDuration([1,4], 2)
+    4
+    >>> findPoisonedDuration([1,2], 2)
+    3
+    """
+    total_duration = 0
+    for i in range(1, len(timeSeries)):
+        time_delta = timeSeries[i] - timeSeries[i - 1]
+        total_duration += min(duration, time_delta)
+    return total_duration + duration
+
+
 # %% 587. Erect the Fence https://leetcode.com/problems/erect-the-fence/
 # Lessons learned:
 # - A broad class of computational geometry algorithms solve this: https://en.wikipedia.org/wiki/Convex_hull_algorithms#Algorithms
