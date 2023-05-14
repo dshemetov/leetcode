a3476f42b9f65857fe6580a84343874e5173d3d2
Dmitry Shemetov <dshemetov@ucdavis.edu>
2023-05-13 23:18:05 -0700

    feat: solve problem 1572

--- solutions.py
+++ solutions.py
@@ -2381,6 +2381,32 @@ def makeGood(s: str) -> str:
     return "".join(stack)
 
 
+# %% 1572. Matrix Diagonal Sum https://leetcode.com/problems/matrix-diagonal-sum/
+# Lessons learned:
+def diagonalSum(mat: list[list[int]]) -> int:
+    """
+    Examples:
+    >>> diagonalSum([[1,2,3],[4,5,6],[7,8,9]])
+    25
+    >>> diagonalSum([[1,1,1,1],[1,1,1,1],[1,1,1,1],[1,1,1,1]])
+    8
+    """
+    n = len(mat)
+
+    if n == 1:
+        return mat[0][0]
+
+    total = 0
+
+    for i in range(n):
+        total += mat[i][i] + mat[n - 1 - i][i]
+
+    if n % 2 == 1:
+        total -= mat[n // 2][n // 2]
+
+    return total
+
+
 # %% 1579. Remove Max Number of Edges to Keep Graph Fully Traversable https://leetcode.com/problems/remove-max-number-of-edges-to-keep-graph-fully-traversable/
 # Lessons learned:
 # - We can build a spanning tree greedily by adding edges when they don't create a cycle. We can detect when
