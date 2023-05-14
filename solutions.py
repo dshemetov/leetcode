8ccb02d124297fcbc1ccc5eeef21ee4d72358149
Dmitry Shemetov <dshemetov@ucdavis.edu>
2023-05-13 23:13:49 -0700

    feat: solve problem 54

--- solutions.py
+++ solutions.py
@@ -699,6 +699,42 @@ def groupAnagrams(strs: list[str]) -> list[list[str]]:
     return list(groups.values())
 
 
+# %% 54. Spiral Matrix https://leetcode.com/problems/spiral-matrix/
+def spiralOrder(matrix: list[list[int]]) -> list[int]:
+    """
+    Examples:
+    >>> spiralOrder([[1,2,3],[4,5,6],[7,8,9]])
+    [1, 2, 3, 6, 9, 8, 7, 4, 5]
+    >>> spiralOrder([[1,2,3,4],[5,6,7,8],[9,10,11,12]])
+    [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]
+    >>> spiralOrder([[1]])
+    [1]
+    >>> spiralOrder([[1,2],[3,4]])
+    [1, 2, 4, 3]
+    """
+    m, n = len(matrix), len(matrix[0])
+    directions = cycle([[0, 1], [1, 0], [0, -1], [-1, 0]])
+    cur_pos, steps = (0, 0), 1
+    visited, elements = set(), [matrix[0][0]]
+    visited.add(cur_pos)
+    for dm, dn in directions:
+        if steps == m * n:
+            break
+
+        while True:
+            new_pos = (cur_pos[0] + dm, cur_pos[1] + dn)
+
+            if new_pos in visited or not (0 <= new_pos[0] < m and 0 <= new_pos[1] < n):
+                break
+
+            cur_pos = new_pos
+            elements.append(matrix[cur_pos[0]][cur_pos[1]])
+            visited.add(cur_pos)
+            steps += 1
+
+    return elements
+
+
 # %% 91. Decode Ways https://leetcode.com/problems/decode-ways/
 # Lessons learned:
 # - My first observation was that characters in "34567890" acted as separators, where I could split the string
