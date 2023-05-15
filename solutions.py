a03f1245d88bcbc2b93218e4d5e5d6aeaabc3b24
Dmitry Shemetov <dshemetov@ucdavis.edu>
2023-05-14 20:59:55 -0700

    feat: solve problem 12

--- solutions.py
+++ solutions.py
@@ -571,6 +571,44 @@ def max_area(height: list[int]) -> int:
     return m
 
 
+# %% 12. Integer to Roman https://leetcode.com/problems/integer-to-roman/
+def int_to_roman(num: int) -> str:
+    """
+    Examples:
+    >>> int_to_roman(3)
+    'III'
+    >>> int_to_roman(4)
+    'IV'
+    >>> int_to_roman(9)
+    'IX'
+    >>> int_to_roman(58)
+    'LVIII'
+    >>> int_to_roman(1994)
+    'MCMXCIV'
+    """
+    letter_map = {
+        1: "I",
+        4: "IV",
+        5: "V",
+        9: "IX",
+        10: "X",
+        40: "XL",
+        50: "L",
+        90: "XC",
+        100: "C",
+        400: "CD",
+        500: "D",
+        900: "CM",
+        1000: "M"
+    }
+    s = ""
+    for k in sorted(letter_map.keys(), reverse=True):
+        r, num = divmod(num, k)
+        s += letter_map[k] * r
+
+    return s
+
+
 # %% 13. Roman to Integer https://leetcode.com/problems/roman-to-integer/
 def roman_to_int(s: str) -> int:
     """
